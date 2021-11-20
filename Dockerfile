468c75a1636aa98f9141591ad851c7add14d6d51
Amr Hassan <amrh434@gmail.com>
2021-11-20 14:23:08 +0200

    fix : migration files + added docker compose for the postgres.

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+
+FROM node:12.13.0-alpine
+RUN mkdir -p /opt/app
+WORKDIR /opt/app
+RUN adduser -S app
+COPY . .
+RUN npm install
+RUN chown -R app /opt/app
+USER app
+EXPOSE 3000
+
+RUN npm run migrate
+CMD [ "npm", "start" ]
--- migrations/sqls/20211108110436-order-products-table-up.sql
+++ migrations/sqls/20211108110436-order-products-table-up.sql
@@ -1,6 +1,13 @@
-CREATE TABLE order_products{
-id SERIAL PRIMARY KEY ,
-order_id bigint references  orders(id),
-product_id bigint references  products(id)
-quantity integer not null
-}
+CREATE TABLE order_products
+(
+    id         SERIAL PRIMARY KEY,
+    order_id   bigint references orders
+        (
+         id
+            ),
+    product_id bigint references products
+        (
+         id
+            ),
+    quantity   integer not null
+)
